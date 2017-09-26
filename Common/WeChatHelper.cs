767a652464d97c5bcfd1478576b6ae8ccc275766
kaiser2012 <kaiser_chen@feiliks.com>
2017-09-26 17:27:20 +0800

    新增文件
    1

---
+++ Common/WeChatHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace WeChat.Common
+{
+    public class WeChatHelper
+    {
+        public static string loadUrl(string url,string data)
+        {
+            try
+            {
+                HttpWebRequest hwr = WebRequest.Create(url) as HttpWebRequest;
+                hwr.Method = "POST";
+                hwr.ContentType = "application/x-www-form-urlencoded";
+                byte[] payload;
+                payload = System.Text.Encoding.UTF8.GetBytes(data);
+                hwr.ContentLength = payload.Length;
+                Stream writer = hwr.GetRequestStream();
+                writer.Write(payload, 0, payload.Length);
+                writer.Close();
+                var result = hwr.GetResponse() as HttpWebResponse;
+                return result.StatusCode.ToString() + "_" + DateTime.Now;
+
+            }
+            catch (Exception e)
+            {
+                LogHelper.Write(e.Message);
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ index.ashx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using WeChat.Common;
+using WeChat.WXModel;
+
+namespace WeChat
+{
+    /// <summary>
+    /// index 的摘要说明
+    /// </summary>
+    public class index : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (TokenModel.token == null)
+            {
+                TokenModel.getAccesss_Token();
+            }
+            createMenu();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 创建菜单
+        /// </summary>
+        private void createMenu()
+        {
+            string data = getMenu();
+            string url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}", TokenHelper.token.access_token);
+            WeChatHelper.loadUrl(url, data);
+            LogHelper.Write(TokenModel.token.access_token);
+        }
+        /// <summary>
+        /// 获取菜单数据
+        /// </summary>
+        /// <returns></returns>
+        private string getMenu()
+        {
+            FileStream fs1 = new FileStream(System.Web.HttpContext.Current.Server.MapPath(".") + "\\file\\menu.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs1, Encoding.GetEncoding("GBK"));
+            string menu = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            return menu;
+        }
+    }
+}
